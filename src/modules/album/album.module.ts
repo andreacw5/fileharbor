50e710979b73b560afb3eb30e169c892b88ca85f
Andrea Tombolato <andreacw96@gmail.com>
2026-01-15 00:46:30 +0100

    Update deps

--- src/modules/album/album.module.ts
+++ src/modules/album/album.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
 import { AlbumService } from './album.service';
 import { AlbumController } from './album.controller';
 import { ClientModule } from '../client/client.module';
@@ -8,15 +6,6 @@ import { ClientModule } from '../client/client.module';
 @Module({
   imports: [
     ClientModule,
-    JwtModule.registerAsync({
-      useFactory: (config: ConfigService) => ({
-        secret: config.get<string>('JWT_SECRET'),
-        signOptions: {
-          expiresIn: config.get<string>('JWT_EXPIRES_IN') || '7d',
-        },
-      }),
-      inject: [ConfigService],
-    }),
   ],
   controllers: [AlbumController],
   providers: [AlbumService],
