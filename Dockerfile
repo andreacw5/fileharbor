a9aa9a7dc6126114da54856cedc299fd35003969
Andrea Tombolato <andreacw96@gmail.com>
2026-01-15 16:39:24 +0100

    FIxed build prisma errors

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:23.11.0-alpine3.21 AS base
+FROM node:24.13.0-alpine3.23 AS base
 
 ENV NODE_ENV=build
 
--- src/modules/prisma/prisma.service.ts
+++ src/modules/prisma/prisma.service.ts
@@ -12,12 +12,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
   }
 
   async onModuleInit() {
-    try {
-      await this.$connect();
-    } catch (error) {
-      console.error('Failed to connect to database:', error);
-      throw error;
-    }
+    await this.$connect();
   }
 
   async onModuleDestroy() {
